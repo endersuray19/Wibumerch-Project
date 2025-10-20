d1cb6b3975e790b201faf518bbccbed9f1759318
endersuray19 <endersurya19@gmail.com>
2025-10-20 09:51:21 +0800

    test auth

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -5,17 +5,28 @@ const page = async () => {
     const { getToken } = await auth();
     const token = await getToken();
     console.log('Token:', token);
+    //product service test
     const res = await fetch('http://localhost:8000/test', {
         headers: {
             Authorization: `Bearer ${token}`
         }
     }
     );
+    
     const data = await res.json();
     console.log(data);
+    //order service test
+    const resProduct = await fetch('http://localhost:8001/test', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    );
+    const dataProduct = await resProduct.json();
+    console.log(dataProduct);
     return (
         <div>
-
+            <h1>Test Page</h1>
         </div>
     )
 }
