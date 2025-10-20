fdbb8da589eff269a7c719dd161f1ff910760290
endersuray19 <endersurya19@gmail.com>
2025-10-20 09:51:33 +0800

    auth order service

--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,9 +1,19 @@
+import Clerk  from '@clerk/fastify';
 import Fastify from 'fastify';
 
 const fastify = Fastify();
 
+fastify.register(Clerk.clerkPlugin)
+
 fastify.get('/',(request,reply)=>{
   return reply.send('Hello from Order Service!')
+})
+fastify.get('/test',(request,reply)=>{
+  const userId = Clerk.getAuth(request);
+  if (!userId.userId) {
+      return reply.status(401).send({ message: 'You Are not Login' });
+  }
+  return reply.send({message:'Hello from Order Service! and you are logged in.'})
 })
 fastify.get('/health', (request,reply) => {
     return reply.status(200).send({
